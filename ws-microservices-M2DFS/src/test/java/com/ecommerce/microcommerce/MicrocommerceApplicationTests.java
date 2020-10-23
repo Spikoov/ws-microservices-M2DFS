e61a89c79bac5e812cdf49171a46c36ae13c8d86
Spikoov <hem.jyh@gmail.com>
2020-10-23 21:04:21 +0200

    Partie 6 / eureka client fonctionne sur circuit breaker, mais pas microcommerce ( je suppose que c'est la version Spring )

--- ws-microservices-M2DFS/src/test/java/com/ecommerce/microcommerce/MicrocommerceApplicationTests.java
+++ ws-microservices-M2DFS/src/test/java/com/ecommerce/microcommerce/MicrocommerceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ecommerce.microcommerce;
 
+//import org.testng.annotations.Test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
